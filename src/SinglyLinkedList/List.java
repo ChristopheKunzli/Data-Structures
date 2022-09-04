144696c7b3c7988d93c8a9f9d79320cc71cfe76f
ChristopheKunzli <christophe.kunzli@cpnv.ch>
2022-09-04 16:15:37 +0200

    add methods to SinglyLinkedList class

--- src/SinglyLinkedList/List.java
+++ src/SinglyLinkedList/List.java
@@ -51,4 +51,66 @@ public class List {
             current = current.next;
         }
     }
+
+    public void printValues(Node head) {
+        if (head == null) return;
+        System.out.print(head.value + " ");
+        printValues(head.next);
+    }
+
+    public int sum() {
+        int sum = 0;
+        Node current = this.head;
+        while (current != null) {
+            sum += current.value;
+            current = current.next;
+        }
+        return sum;
+    }
+
+    public int sum(Node head) {
+        if (head == null) return 0;
+        return head.value + sum(head.next);
+    }
+
+    public boolean includes(int target) {
+        Node current = this.head;
+        while (current != null) {
+            if (current.value == target) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public boolean includes(Node head, int target) {
+        if (head == null) return false;
+        if (head.value == target) return true;
+        return includes(head.next, target);
+    }
+
+    public int getValue(int targetIndex) {
+        Node current = this.head;
+        int index = 0;
+        while (index != targetIndex) {
+            if(current.next == null) throw new IndexOutOfBoundsException();
+
+            current = current.next;
+            index++;
+        }
+        return current.value;
+    }
+
+    public Node reverseList(){
+        Node previous = null;
+        Node current = this.head;
+
+        while (current != null){
+            Node next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        this.head = previous;
+        return previous;
+    }
 }
